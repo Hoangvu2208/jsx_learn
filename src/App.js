2ad42abf11c10e742aede8afb43bd86732f34531
Hoangvu2208 <vudinhhoang2know@gmail.com>
2023-04-23 16:21:40 +0700

    update

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
+
 import './App.css';
+import LoginForm from './SignInForm/LoginForm';
+import BrowserInfo from './browserInfo/BrowserInfo';
+import Avatar from './profile/Avatar';
+import StudentList from './studentList/StudentList';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* <BrowserInfo /> */}
+      {/* <StudentList /> */}
+      {/* <Avatar /> */}
+      {/* <LoginForm /> */}
     </div>
   );
 }
---
+++ src/browserInfo/BrowserInfo.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const BrowserInfo = () => {
+  return (
+    <h4>BrowserInfo : {navigator.userAgent}</h4>
+  )
+}
+
+export default BrowserInfo
